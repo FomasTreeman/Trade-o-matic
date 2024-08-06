f090b28436791a3a3302817ad25f46c3ea422954
Thomas Freeman <tom@team-freeman.com>
2024-08-06 15:56:21 +0100

    hide api keys

--- src/main/java/dev/fomastree/App.java
+++ src/main/java/dev/fomastree/App.java
@@ -2,11 +2,19 @@ package dev.fomastree;
 
 import com.google.gson.JsonArray;
 
+import dev.fomastree.backend.Alpaca;
 import dev.fomastree.backend.JSON;
 import dev.fomastree.strategies.StrategyFactory;
 
 public class App {
     public static void main(String[] args) {
+        
+        System.out.println(args.length);
+
+        if (args.length > 0) {
+            Alpaca.getHistoricalData(args[0]);
+        }
+
         JsonArray array = JSON.readJSON("AAPL", "bars");
         StrategyFactory strategyFactory = new StrategyFactory();
         strategyFactory.useStrategy("Bollinger").backTest(array, "AAPL");
--- src/main/java/dev/fomastree/backend/Alpaca.java
+++ src/main/java/dev/fomastree/backend/Alpaca.java
@@ -14,13 +14,14 @@ public class Alpaca {
 
         Request request = new Request.Builder()
                 .url(
-                        "https://data.alpaca.markets/v2/stocks/" + symbol.toLowerCase() + "/bars?timeframe=1D&start=2023-08-01T00%3A00%3A00Z&end=2024-08-01T00%3A00%3A00Z&limit=1000&adjustment=raw&feed=sip&currency=GBP&sort=asc")
+                        "https://data.alpaca.markets/v2/stocks/" + symbol.toLowerCase()
+                                + "/bars?timeframe=1D&start=2023-08-01T00%3A00%3A00Z&end=2024-08-01T00%3A00%3A00Z&limit=1000&adjustment=raw&feed=sip&currency=GBP&sort=asc")
                 .get()
                 .addHeader("accept", "application/json")
-                .addHeader("APCA-API-KEY-ID", "System.getenv("APCA_API_KEY_ID")")
+                .addHeader("APCA-API-KEY-ID", System.getenv("APCA_API_KEY_ID"))
                 .addHeader(
                         "APCA-API-SECRET-KEY",
-                        "System.getenv("APCA_API_SECRET_KEY")")
+                        System.getenv("APCA_API_SECRET_KEY"))
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
@@ -45,11 +46,11 @@ public class Alpaca {
     }
 
     // private static class DateTimeUtils {
-    //     public static String getCurrentDateTimeSmallFormat() {
-    //         LocalDateTime currentDateTime = LocalDateTime.now();
-    //         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-    //                 "MM-dd-yy-HH:mm:ss");
-    //         return currentDateTime.format(formatter);
-    //     }
+    // public static String getCurrentDateTimeSmallFormat() {
+    // LocalDateTime currentDateTime = LocalDateTime.now();
+    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+    // "MM-dd-yy-HH:mm:ss");
+    // return currentDateTime.format(formatter);
+    // }
     // }
 }

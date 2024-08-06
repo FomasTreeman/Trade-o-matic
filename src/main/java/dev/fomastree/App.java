8d6fdd9fd7f7e075f190cdec9993dc2733795b67
Thomas Freeman <tom@team-freeman.com>
2024-08-06 15:24:00 +0100

    remove history data from git

--- src/main/java/dev/fomastree/App.java
+++ src/main/java/dev/fomastree/App.java
@@ -1,18 +1,8 @@
 package dev.fomastree;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
-import dev.fomastree.backend.Bar;
 import dev.fomastree.backend.JSON;
-import dev.fomastree.strategies.Strategy;
 import dev.fomastree.strategies.StrategyFactory;
 
 public class App {
